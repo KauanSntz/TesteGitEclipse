da00c8241f0d60dcd65d2498535d10bf6f11b486
Kauan Ferreira dos Santos <113863647+KauanSntz@users.noreply.github.com>
2022-09-18 19:48:09 -0400

    Criei a main e dei oi ao GitHub

---
+++ TesteGitHub/src/CodigoAleatorio/HelloGIt.java
@@ -0,0 +1,13 @@
+package CodigoAleatorio;
+
+// Kauan Santos - GitHub // 18/09/2022
+
+public class HelloGIt {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, GitHub!");
+
+	}
+
+}
